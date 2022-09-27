640fcc7b8f98b068d0716d6604767c055cacc70e
learnWithZak <learn.with.zak@gomail.com>
2022-09-27 20:03:28 +0200

    refactor: modify kmp config

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+
 plugins {
     kotlin("multiplatform")
     id("com.android.library")
@@ -5,7 +7,13 @@ plugins {
 
 kotlin {
     android()
-
+    ios {
+        binaries {
+            framework {
+                baseName = "shared"
+            }
+        }
+    }
     sourceSets {
         val commonMain by getting
         val androidMain by getting {
@@ -13,6 +21,7 @@ kotlin {
                 implementation("com.google.android.material:material:1.2.1")
             }
         }
+        val iosMain by getting
     }
 }
 
@@ -24,3 +33,28 @@ android {
         targetSdkVersion(29)
     }
 }
+
+val packForXcode by tasks.creating(Sync::class) {
+    group = "build"
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
+    val framework = kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
+    inputs.property("mode", mode)
+    dependsOn(framework.linkTask)
+    val targetDir = File(buildDir, "xcode-frameworks")
+    from({ framework.outputDirectory })
+    into(targetDir)
+    /// generate a helpful ./gradlew wrapper with embedded Java path
+    doLast {
+        val gradlew = File(targetDir, "gradlew")
+        gradlew.writeText("#!/bin/bash\n"
+                + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
+                + "cd '${rootProject.rootDir}'\n"
+                + "./gradlew \$@\n")
+        gradlew.setExecutable(true)
+    }
+
+}
+
+tasks.getByName("build").dependsOn(packForXcode)
